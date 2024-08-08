6a4aed01160c25db7a0dac9ca245eace84033ab4
aljawa <alecw01@protonmail.com>
2024-08-08 12:50:31 -0600

    add get_char_counts function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    print(get_char_counts(text))
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +12,16 @@ def get_book_text(path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_char_counts(text):
+    lowered = text.lower()
+    chars = {}
+    
+    for char in lowered:
+        if char not in chars.keys():
+            chars[char] = 1
+        else: 
+            chars[char] += 1
+    return chars
 
 main()
