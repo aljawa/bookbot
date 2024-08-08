e4cb7ee5c1923ed1facb48535f8a22c94b0576b1
aljawa <alecw01@protonmail.com>
2024-08-08 13:29:41 -0600

    add get_sorted_list and sort_on functions

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars = get_char_counts(text)
+    sorted_chars = get_sorted_list(chars)
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
-    print(get_char_counts(text))
+    print()
+
+    for item in sorted_chars:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -23,5 +33,15 @@ def get_char_counts(text):
         else: 
             chars[char] += 1
     return chars
+
+def sort_on(d):
+    return d["num"]
+
+def get_sorted_list(d):
+    sorted_list = []
+    for char in d:
+        sorted_list.append({"char": char, "num": d[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 main()
